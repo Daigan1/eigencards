38f03437b255cb68804dabbe431372aaf52f6f6f
Daigan Berger <daiganberger@gmail.com>
2025-02-24 23:08:48 -0700

    fix unauth

--- front-end/src/app/unauthenticated/page.js
+++ front-end/src/app/unauthenticated/page.js
@@ -1,13 +1,9 @@
 'use client'
 
-import Header from "../components/header";
-import Card from "../components/card";
-import Profile from "../components/profile";
 
 export default function Home() {
 	return (
 		<div>
-			<Header />
 				<div className = "w-full h-[85vh]">
 					<div className = "flex justify-center items-center h-full">
 			
